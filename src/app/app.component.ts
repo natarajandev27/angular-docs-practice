0084b0a16e9d860d4cfff8c9886ae27347e4372d
Natarajan M <natarajan27061996@gmail.com>
2026-06-26 13:05:40 +0530

    docs: component anatomy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProfilePhotoComponent } from './features/components/profile-photo/profile-photo.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ProfilePhotoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
