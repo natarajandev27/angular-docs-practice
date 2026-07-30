a8805b020287b59c581f30052843b31b63074705
Natarajan M <natarajan27061996@gmail.com>
2026-07-30 20:45:29 +0530

    rxjs: subjects

--- src/app/features/rxjs/operators/operators.component.ts
+++ src/app/features/rxjs/operators/operators.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {
+  AsyncSubject,
+  BehaviorSubject,
   combineLatest,
   concatMap,
   debounceTime,
@@ -11,6 +13,7 @@ import {
   merge,
   mergeMap,
   of,
+  ReplaySubject,
   Subject,
   switchMap,
   tap,
@@ -24,7 +27,6 @@ import {
 })
 export class OperatorsComponent {
   // serachTerms$ = new Subject<string>();
-
   // constructor() {
   //   this.serachTerms$
   //     .pipe(
@@ -38,14 +40,11 @@ export class OperatorsComponent {
   //     )
   //     .subscribe((result) => console.log('Got:', result));
   // }
-
   // onType(event: Event) {
   //   const value = (event.target as HTMLInputElement).value;
   //   this.serachTerms$.next(value);
   // }
-
   // private buttonClick$ = new Subject<void>();
-
   // constructor() {
   //   this.buttonClick$
   //     .pipe(
@@ -56,11 +55,9 @@ export class OperatorsComponent {
   //     )
   //     .subscribe((result) => console.log('GOT:', result));
   // }
-
   // onClick() {
   //   this.buttonClick$.next();
   // }
-
   // testMapAndTap() {
   //   of(1, 2, 3)
   //     .pipe(
@@ -70,63 +67,86 @@ export class OperatorsComponent {
   //     )
   //     .subscribe((finalVal) => console.log('Subscribed value:', finalVal));
   // }
-
   // Combination Operators
+  // constructor() {
+  //   // this.testForkJoin();
+  //   // this.testCombineLatest();
+  //   this.testMerge();
+  // }
+  // testForkJoin() {
+  //   const call1$ = of('User data').pipe(delay(1000));
+  //   const call2$ = of('Posts data').pipe(delay(1000));
+  //   const call3$ = of('Settings data').pipe(delay(1000));
+  //   console.log('Starting all 3 calls...');
+  //   forkJoin({
+  //     user: call1$,
+  //     post: call2$,
+  //     settings: call3$,
+  //   }).subscribe((result) => {
+  //     console.log('All done:', result);
+  //   });
+  // }
+  // testCombineLatest() {
+  //   const price$ = new Subject<number>();
+  //   const quantity$ = new Subject<number>();
+  //   combineLatest([price$, quantity$]).subscribe(([price, quantity]) => {
+  //     console.log('Total:', price * quantity);
+  //   });
+  //   console.log('Emitting price = 10');
+  //   price$.next(10);
+  //   console.log('Emitting quantity = 2');
+  //   quantity$.next(2);
+  //   console.log('Emitting quantity = 15');
+  //   price$.next(15);
+  // }
+  // testMerge() {
+  //   const buttonClick$ = new Subject<string>();
+  //   const enterKeyPress$ = new Subject<string>();
+  //   merge(buttonClick$, enterKeyPress$).subscribe((source) => {
+  //     console.log('Triggered by:', source);
+  //   });
+  //   console.log('Simulating button click');
+  //   buttonClick$.next('button');
+  //   console.log('Simulating Enter key press');
+  //   enterKeyPress$.next('enter key');
+  //   console.log('Simulating another button click');
+  //   buttonClick$.next('button');
+  // }
+
+  // Subjects
   constructor() {
-    // this.testForkJoin();
-    // this.testCombineLatest();
-    this.testMerge();
+    this.testSubjectTypes();
   }
 
-  testForkJoin() {
-    const call1$ = of('User data').pipe(delay(1000));
-    const call2$ = of('Posts data').pipe(delay(1000));
-    const call3$ = of('Settings data').pipe(delay(1000));
+  testSubjectTypes() {
+    const plain$ = new Subject<number>();
+    plain$.next(1);
+    plain$.next(2);
+    console.log('--- Plain Subject: subscribing LATE ---');
+    plain$.subscribe((val) => console.log('Plain got:', val));
+    plain$.next(3);
 
-    console.log('Starting all 3 calls...');
+    const behavior$ = new BehaviorSubject<number>(0);
+    behavior$.next(1);
+    behavior$.next(2);
+    console.log('--- BehaviorSubject Subject: subscribing LATE ---');
+    behavior$.subscribe((val) => console.log('BehaviorSubject got:', val));
+    behavior$.next(3);
 
-    forkJoin({
-      user: call1$,
-      post: call2$,
-      settings: call3$,
-    }).subscribe((result) => {
-      console.log('All done:', result);
-    });
-  }
+    const replay$ = new ReplaySubject<number>(1);
+    replay$.next(1);
+    replay$.next(2);
+    replay$.next(3);
+    console.log('--- ReplaySubject Subject: subscribing LATE ---');
+    replay$.subscribe((val) => console.log('ReplaySubject got:', val));
 
-  testCombineLatest() {
-    const price$ = new Subject<number>();
-    const quantity$ = new Subject<number>();
-
-    combineLatest([price$, quantity$]).subscribe(([price, quantity]) => {
-      console.log('Total:', price * quantity);
-    });
-
-    console.log('Emitting price = 10');
-    price$.next(10);
-
-    console.log('Emitting quantity = 2');
-    quantity$.next(2);
-
-    console.log('Emitting quantity = 15');
-    price$.next(15);
-  }
-
-  testMerge() {
-    const buttonClick$ = new Subject<string>();
-    const enterKeyPress$ = new Subject<string>();
-
-    merge(buttonClick$, enterKeyPress$).subscribe((source) => {
-      console.log('Triggered by:', source);
-    });
-
-    console.log('Simulating button click');
-    buttonClick$.next('button');
-
-    console.log('Simulating Enter key press');
-    enterKeyPress$.next('enter key');
-
-    console.log('Simulating another button click');
-    buttonClick$.next('button');
+    const async$ = new AsyncSubject<number>();
+    console.log('--- ReplaySubject Subject: subscribing LATE ---');
+    async$.subscribe((val) => console.log('Async got:', val));
+    async$.next(1);
+    async$.next(2);
+    async$.next(3);
+    console.log('--- Calling complete() now ---');
+    async$.complete();
   }
 }
